3470d8739e2ec14fe5c23b053a09cfe6a5975c1b
unknown <devinbrewer2000@gmail.com>
2018-10-07 19:40:51 -0600

    Competed Part 1

--- lab3-dbrewer5.cpp
+++ lab3-dbrewer5.cpp
@@ -4,9 +4,22 @@
 #include <iostream>
 
 int main() {
+  /*  Part #1  */
+  short n, sum=0;
 
-  std::cout << "It lives\n";
+  // Get a value for n to compute the sigma(i=1, n, i) of
+  std::cout << "Enter a value for 'n': ";
+  std::cin >> n;
 
-  system("pause");
+  // Calculate the sigma(i=1, n, i)
+  for (short i = 1; i <= n; i++) {  /*NOTE: A value of n > 255 will cause sum to overflow*/
+    sum += i;
+  }
+
+  // Output the result of the function
+  std::cout << "n = " << n << "\n";
+  std::cout << "sum = " << sum << "\n";
+
+
   return 0;
 }
